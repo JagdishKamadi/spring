2e49806435a92e894fdbacfbeadac24c1ff65a7a
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2025-05-04 09:41:38 +0530

    Spring Core Xml Config: Updated the mix config like xml and annotation

--- spring core/spring-core-xml-config-demo/src/main/java/com/epam/service/TwitterService.java
+++ spring core/spring-core-xml-config-demo/src/main/java/com/epam/service/TwitterService.java
@@ -1,6 +1,14 @@
 package com.epam.service;
 
+import org.springframework.stereotype.Component;
+
+@Component("twitterServiceCustomBean")
 public class TwitterService implements MessageService {
+
+    public TwitterService() {
+        System.out.println("TwitterService bean created");
+    }
+
     @Override
     public void processMessage(String message) {
         System.out.println("In class TwitterService");
