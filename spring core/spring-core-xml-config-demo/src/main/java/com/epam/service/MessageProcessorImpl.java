55b79fe38d7827645961d6e35afe69716b011015
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2025-05-04 08:53:07 +0530

    Spring Core Xml Config: Updated the simple setter based configuration

--- spring core/spring-core-xml-config-demo/src/main/java/com/epam/service/MessageProcessorImpl.java
+++ spring core/spring-core-xml-config-demo/src/main/java/com/epam/service/MessageProcessorImpl.java
@@ -4,8 +4,10 @@ public class MessageProcessorImpl implements MessageProcessor {
 
     private final MessageService twitterService;
     private final MessageService whatsAppService;
+    private MessageService messageService;
 
     public MessageProcessorImpl(MessageService twitterService, MessageService whatsAppService) {
+        System.out.println("Calling the constructor injection");
         this.twitterService = twitterService;
         this.whatsAppService = whatsAppService;
     }
@@ -14,5 +16,11 @@ public class MessageProcessorImpl implements MessageProcessor {
     public void processMessage(String message) {
         twitterService.processMessage(message);
         whatsAppService.processMessage(message);
+        messageService.processMessage(message);
+    }
+
+    public void setMessageService(MessageService messageService) {
+        System.out.println("Calling the setter injection");
+        this.messageService = messageService;
     }
 }
